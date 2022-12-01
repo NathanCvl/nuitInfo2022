7df884cd54fede389f1803bb671aaf0357b0eadd
nathancuvelier <nathancuvelier59@icloud.com>
2022-12-01 03:14:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// design file
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// routers
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// server listening
+app.listen(PORT, () => {
+  console.log(`The app start on http://localhost:${PORT}`);
+});
